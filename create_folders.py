787d4784eb300d0e606fd5d6b8187c1870837198
Bruno <el_diablo76@msn.com>
2024-05-06 06:23:03 +0200

    modified:   create_folders.py

--- create_folders.py
+++ create_folders.py
@@ -61,6 +61,7 @@ def create_folder(start_date, end_date, current_folder, subfolders, checkbox):
             index_folder += 2
             week_num = current_date.isocalendar()[1]
         else:
+            checkbox.setChecked(False)
             path, index_prime = manage_directory_creation(
                 current_date, folder_num, index_folder, index_prime,
                 current_folder, subfolders)
